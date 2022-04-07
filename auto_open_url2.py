e9058da7523bfca00cb4145ad7b7ae1ebabb513d
Mark <willingwind@icloud.com>
2022-04-07 22:38:36 +0800

    m

--- auto_open_url2.py
+++ auto_open_url2.py
@@ -8,8 +8,12 @@ scheduler=BackgroundScheduler()
 def auto_open_url():
     webbrowser.open('https://soft.sorning.com')
     print(time.asctime(time.localtime(time.time())))
+def auto():
+    webbrowser.open('https://willingwoods.com')
+    print(time.asctime(time.localtime(time.time())))
 
-scheduler.add_job(auto_open_url, trigger='interval', id='3', seconds=5)
+scheduler.add_job(auto_open_url, trigger='interval', id='3', hours=5, jitter=60*60)
+scheduler.add_job(auto, 'interval', hours=3, jitter=60*60)
 scheduler.start()
 
 try:
